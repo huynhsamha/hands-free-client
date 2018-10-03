702f99a82ed8633ba6945b027d4e3c6475edd82d
Huynh Ha <huynhsamha@gmail.com>
2018-10-03 13:29:31 +0700

    add products index page

--- src/database/deal_products.js
+++ src/database/deal_products.js
@@ -1,60 +1,56 @@
 import numeral from 'numeral';
+import moment from 'moment';
 
 export const dealProducts = [
   {
     brand: 'Apple',
     name: 'Apple iPhone X 64GB',
     ceilprice: 23700000,
-    percent: 15,
     dealprice: 20140000,
     sold: 86,
     avai: 125,
-    dealtime: new Date(2018, 9, 1, 20, 30),
-    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/x/-/x-gray.jpg'
+    dealtime: moment().add(2, 'hours').add(35, 'minutes'),
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/x/-/x-gray.jpg'
   },
   {
     brand: 'Samsung',
     name: 'Samsung Galaxy Note 9 Chính hãng',
     ceilprice: 22990000,
-    percent: 10,
     dealprice: 20690000,
     sold: 92,
     avai: 65,
-    dealtime: new Date(2018, 9, 1, 23),
-    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/g/a/galaxy-note-9-bronze.jpg'
+    dealtime: moment().add(5, 'hours').add(10, 'minutes'),
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/g/a/galaxy-note-9-bronze.jpg'
   },
   {
     brand: 'Xiaomi',
     name: 'Xiaomi Redmi Note 5 32GB Chính hãng',
     ceilprice: 4350000,
-    percent: 10,
     dealprice: 3915000,
     sold: 265,
     avai: 136,
-    dealtime: new Date(2018, 9, 1, 18),
-    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/r/e/redmi-note-5-pro-black_1.jpg'
+    dealtime: moment().add(15, 'hours').add(15, 'minutes'),
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/r/e/redmi-note-5-pro-1.jpg'
   },
   {
     brand: 'Huawei',
     name: 'Huawei nova 3i Chính hãng',
     ceilprice: 7990000,
-    percent: 30,
     dealprice: 5593000,
     sold: 356,
     avai: 82,
-    dealtime: new Date(2018, 9, 1, 11),
-    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/n/o/nova-3i-white.jpg'
+    dealtime: moment().add(30, 'hours').add(45, 'minutes'),
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/n/o/nova-3i-white.jpg'
   },
   {
     brand: 'OPPO',
     name: 'OPPO A3s 16GB Chính hãng',
     ceilprice: 3885000,
-    percent: 20,
     dealprice: 3108000,
     sold: 202,
     avai: 36,
-    dealtime: new Date(2018, 9, 1, 14),
-    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/small_image/220x175/9df78eab33525d08d6e5fb8d27136e95/a/3/a3s-purple.jpg'
+    dealtime: moment().add(25, 'hours').add(50, 'minutes'),
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/a/3/a3s-purple.jpg'
   }
 ];
 
@@ -81,7 +77,7 @@ export const renderDealProducts = item => `
                   </div>
                   <div class="deals_timer d-flex flex-row align-items-center justify-content-start">
                     <div class="deals_timer_title_container">
-                      <div class="deals_timer_title">Nhanh tay nào</div>
+                      <div class="deals_timer_title">Duy nhất</div>
                       <div class="deals_timer_subtitle">Chỉ còn:</div>
                     </div>
                     <div class="deals_timer_content ml-auto">
---
+++ src/database/product_panel.js
@@ -0,0 +1,121 @@
+import numeral from 'numeral';
+
+export const listProducts = [
+  {
+    brand: 'Apple',
+    name: 'Apple iPhone XS Max 64GB 2 SIM',
+    ceilprice: 31100000,
+    dealprice: 26430000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/x/s/xs_1_7.png'
+  },
+  {
+    brand: 'Samsung',
+    name: 'Samsung Galaxy Note 8 Chính hãng',
+    ceilprice: 14790000,
+    dealprice: 133000000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/n/o/note-8-4.jpg'
+  },
+  {
+    brand: 'OPPO',
+    name: 'OPPO Find X Chính hãng',
+    ceilprice: 20990000,
+    dealprice: 15749000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/f/i/find-x-1_1.jpg'
+  },
+  {
+    brand: 'Xiaomi',
+    name: 'Xiaomi Mi 8 Explorer Edition 128 GB chính hãng',
+    ceilprice: 12000000,
+    dealprice: 10560000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/m/i/mi-8-black_2_1.jpg'
+  },
+  {
+    brand: 'Huawei',
+    name: 'Huawei P20 Pro Chính hãng',
+    ceilprice: 18000000,
+    dealprice: 16200000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/p/2/p20-pro.jpg'
+  },
+  {
+    brand: 'OPPO',
+    name: 'OPPO F9 Chính hãng',
+    ceilprice: 8569000,
+    dealprice: 7599000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/f/9/f9-red_2.jpg'
+  },
+  {
+    brand: 'Xiaomi',
+    name: 'Xiaomi Mi Max 3 64GB Chính hãng',
+    ceilprice: 7450000,
+    dealprice: 6099000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/m/i/mi-max-3-2.jpg'
+  },
+  {
+    brand: 'Samsung',
+    name: 'Samsung Galaxy S8+ Chính hãng',
+    ceilprice: 17990000,
+    dealprice: 15290000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/s/8/s8_-3.jpg'
+  },
+  {
+    brand: 'Apple',
+    name: 'Apple iPhone XS Max 256GB 2 SIM',
+    ceilprice: 36999000,
+    dealprice: 34039000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/x/s/xs-1_8_5_1.png'
+  },
+  {
+    brand: 'Samsung',
+    name: 'Samsung Galaxy A8+ (Plus) (2018) Chính hãng',
+    ceilprice: 13990000,
+    dealprice: 12730000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/a/8/a8-2018-gray_2.jpg'
+  },
+  {
+    brand: 'Xiaomi',
+    name: 'Xiaomi Mi A1 32GB Chính hãng',
+    ceilprice: 4990000,
+    dealprice: 3650000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/thumbnail/9df78eab33525d08d6e5fb8d27136e95/m/i/mi-a1-2.jpg'
+  },
+  {
+    brand: 'OPPO',
+    name: 'OPPO F7 128GB Chính hãng',
+    ceilprice: 12199000,
+    dealprice: 8990000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/f/7/f7-red_6.jpg'
+  },
+  {
+    brand: 'Nokia',
+    name: 'Nokia 6.1 Plus Chính hãng',
+    ceilprice: 8112000,
+    dealprice: 6599000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/n/o/nokia-6-plus-white_3.jpg'
+  },
+  {
+    brand: 'Huawei',
+    name: 'Huawei Y7 Pro 2018 Chính hãng',
+    ceilprice: 4560000,
+    dealprice: 3990000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/y/7/y7-pro-2018-blue.jpg'
+  },
+  {
+    brand: 'Hãng khác',
+    name: 'Honor 10 Chính hãng',
+    ceilprice: 11590000,
+    dealprice: 9990000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/h/o/honor-10-black_1.jpg'
+  },
+  {
+    brand: 'Hãng khác',
+    name: 'Vivo V7+ Chính hãng',
+    ceilprice: 7999000,
+    dealprice: 6490000,
+    thumbnail: 'https://cellphones.com.vn/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/v/7/v7-plus-black.jpg'
+  }
+];
+
+
+export const renderItemProduct = item => `
+
+`;
